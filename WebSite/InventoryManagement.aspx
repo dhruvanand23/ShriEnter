1805d0eed6fa0c7c6c12ee99024688ae5f9b303d
dhruvanand23 <da23071997@gmail.com>
2021-03-22 17:52:08 +0530

    Final Upload

---
+++ WebSite/InventoryManagement.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/AdminMasterPage.Master" AutoEventWireup="true" CodeFile="InventoryManagement.aspx.cs" Inherits="WebSite.InventoryManagement" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <br />
+    <br />
+    <br />
+
+    <hr />
+    <h2><b>Inventory Manager</b></h2>   
+    <hr />
+    
+    Choose an Supplier: 
+    <asp:DropDownList ID="ddlSupplier" runat="server" AutoPostBack="true" CssClass="form-control" OnSelectedIndexChanged="ddlSupplier_SelectedIndexChanged"></asp:DropDownList>
+
+    Choose an Item: 
+    <asp:DropDownList ID="ddlRawMaterial" runat="server" AutoPostBack="true" CssClass="form-control"></asp:DropDownList>
+
+    <asp:Label ID="qtyDisplay" runat="server" Text="abc"></asp:Label>
+
+
+    <br />
+    <br />
+    <hr />
+</asp:Content>
---
+++ WebSite/InventoryManagement.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+
+namespace WebSite
+{
+    public partial class InventoryManagement : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-GQMSKCM\SQLEXPRESS;Initial Catalog=mydata1;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+            if (!IsPostBack)
+            {
+                BindSupplier();
+                BindRMaterial();
+            }
+        }
+
+        private void BindSupplier()
+        {
+            cmd = new SqlCommand("Select * from tblSupplier", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count != 0)
+            {
+                ddlSupplier.DataSource = dt;
+                ddlSupplier.DataTextField = "SupName";
+                ddlSupplier.DataValueField = "SupID";
+                ddlSupplier.DataBind();
+                ddlSupplier.Items.Insert(0, new ListItem("-Select-", "0"));
+                dt.Dispose();
+
+            }
+        }
+
+        private void BindRMaterial()
+        {
+            cmd = new SqlCommand("Select * from tblRMaterial where SupID=@uname ", con);
+            cmd.Parameters.AddWithValue("@uname", ddlSupplier.SelectedItem.Value);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count != 0)
+            {
+                ddlRawMaterial.DataSource = dt;
+                ddlRawMaterial.DataTextField = "RM_Name";
+                ddlRawMaterial.DataValueField = "RM_ID";
+                ddlRawMaterial.DataBind();
+                ddlRawMaterial.Items.Insert(0, new ListItem("-Select-", "0"));
+                dt.Dispose();
+
+            }
+        }
+
+        protected void ddlSupplier_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BindRMaterial();
+        }
+    }
+}
